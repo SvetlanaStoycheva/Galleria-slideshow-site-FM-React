a44b782bd0dc9039c279fa127993dc0b49bfa50a
Svetlana Stoycheva <s.stoycheva@gmail.com>
2022-03-08 19:04:50 +0200

    homepage mobile

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import logo from './assets/shared/logo.svg';
+import { data } from './data';
 
 function App() {
-  return <h2>hello</h2>;
+  return (
+    <div className='main'>
+      <div className='homepage-header'>
+        <img src={logo} alt='logo' />
+        <p className='start-slideshow'>start slideshow</p>
+      </div>
+      <div className='homepage-line'></div>
+      <section className='gallery-container'>
+        {data.map((item, index) => {
+          const {
+            name,
+            artist: { name: artistName },
+            images: { gallery: galleryImage },
+          } = item;
+
+          return (
+            <div className='single-gallery-image-container' key={index}>
+              <img
+                className='homepage-gallery-image'
+                src={`${galleryImage}`}
+                alt={name}
+              />
+              <div className='single-gallery-image-text'>
+                <h1>{name}</h1>
+                <h3>{artistName}</h3>
+              </div>
+            </div>
+          );
+        })}
+      </section>
+    </div>
+  );
 }
 
 export default App;
