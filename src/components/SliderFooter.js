e816f6b46fe11751984af959c441a934ead3b9a9
Svetlana Stoycheva <s.stoycheva@gmail.com>
2022-03-18 15:56:27 +0200

    sliderPage footer

--- src/components/SliderFooter.js
+++ src/components/SliderFooter.js
@@ -30,17 +30,58 @@ const SliderFooter = () => {
 
   return (
     <footer className='footer-container'>
-      <div className='footer-name-container'>
-        <h3>{name}</h3>
-        <h4>{artistName}</h4>
+      <div className='line'>
+        <div
+          className={
+            data.indexOf(currentSliderImage) < 5
+              ? 'line1 '
+              : data.indexOf(currentSliderImage) > 10
+              ? 'line1-100'
+              : 'line1-30'
+          }
+        ></div>
+        <div
+          className={
+            data.indexOf(currentSliderImage) > 10 ? 'line2-100' : 'line2'
+          }
+        ></div>
       </div>
-      <div className='footer-arrows-container'>
-        <span>
-          <IoPlaySkipBackOutline onClick={rotateImagesDown} />
-        </span>
-        <span>
-          <IoPlaySkipForwardOutline onClick={rotateImagesUp} />
-        </span>
+      <div className='footer-inner-container'>
+        <div className='footer-name-container'>
+          <h3>{name}</h3>
+          <h4>{artistName}</h4>
+        </div>
+        {/* arrow buttons */}
+        <div className='footer-arrows-container'>
+          <button
+            type='button'
+            className='arrow-btn'
+            disabled={data.indexOf(currentSliderImage) === 0}
+          >
+            <IoPlaySkipBackOutline
+              onClick={rotateImagesDown}
+              className={
+                data.indexOf(currentSliderImage) === 0
+                  ? 'arrow-down'
+                  : 'arrow-down arrow-active'
+              }
+            />
+          </button>
+          <button
+            type='button'
+            className='arrow-btn'
+            disabled={data.indexOf(currentSliderImage) === data.length - 1}
+          >
+            <IoPlaySkipForwardOutline
+              onClick={rotateImagesUp}
+              className={
+                data.indexOf(currentSliderImage) === data.length - 1
+                  ? 'arrow-up'
+                  : 'arrow-up arrow-active'
+              }
+            />
+          </button>
+        </div>
       </div>
     </footer>
   );
